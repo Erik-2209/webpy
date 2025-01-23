e274025713007b9aaffcb0e12915e5d97236d800
ERIK HERNANDEZ <156980862+Erik-2209@users.noreply.github.com>
2025-01-23 18:58:36 +0000

    UPDATE

---
+++ 04_webapp/app.py
@@ -0,0 +1,13 @@
+import web
+urls = (
+    '/', 'index'
+    '/bienvenida', 'Bienvenida'
+)
+app = web.application(urls, globals())
+render = web.template.render('views')
+class index :
+    def GET (self):
+        mensaje = "Hola python y html"
+        return render.index(mensaje)
+if __name__ == "__main__":
+    app.run()
